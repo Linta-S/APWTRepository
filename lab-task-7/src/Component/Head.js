fb7c8882dd3bbba1dd4dd05ecc76b5d0f2545cee
Sahara khan Linta <75035777+Linta-S@users.noreply.github.com>
2022-08-06 23:25:37 +0600

    task 7 done

--- lab-task-7/src/Component/Head.js
+++ lab-task-7/src/Component/Head.js
@@ -7,12 +7,7 @@ const Head = () => {
             <Link to="/">Home</Link>
             <Link to="/profile">Profile</Link>
             <Link to="/contact">Contact</Link>
-            <Link to="/color">Color State</Link>
-            <Link to="/effect">Effect State</Link>
-            <Link to="/posts">All Posts</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/allproducts">Products</Link>
-            <Link to="/signout">SignOut</Link>
+      
         </div>
     )
 }
